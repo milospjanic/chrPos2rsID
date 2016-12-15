8830e76fb0d1a118e148ebf59b6d488c33672eef
Milos Pjanic <pjanic.milos@gmail.com>
2016-12-14 17:37:04 -0800

    Update chrPos2rsID.sh

--- chrPos2rsID.sh
+++ chrPos2rsID.sh
@@ -25,12 +25,14 @@ fi
 tabsep $SNPS
 sed 's/^/chr/g' SFJK_regions.IL_16.20161129SIG.txt | sed 's/_/\t/' | sed 's/_.//' | sed 's/_.//' > $SNPS.mod
 sed 's/^MarkerName/chr\tposition\t/g' <(head -n1 $SNPS) > $SNPS.head
-cat $SNPS.head <(tail -n+2 $SNPS.mod) > $1.mod2
-mv $1.mod2 $1.mod
-tabsep $1.mod
-tail -n+2 $1.mod > $1.mod2
-head -n1 $1.mod > $1.head
+cat $SNPS.head <(tail -n+2 $SNPS.mod) > $SNPS.mod2
+mv $SNPS.mod2 $SNPS.mod
+tabsep $SNPS.mod
+tail -n+2 $SNPS.mod > $SNPS.mod2
+head -n1 $SNPS.mod > $SNPS.head
 
 
 #find positions of snps from the input list by comparing to snpdb
-awk 'NR==FNR {h[$1] = 1; next} {if(h[$4]==1) print$0}' $SNPS snp147Common.bed > $1.bed
+awk 'NR==FNR {h1[$1] = 1; h2[$3]=1; h3[$1$3]=$4; next} {if(h2[$2]==1 && h1[$1]==1) print h3[$1$2]"\t"$0}' snp147Common.bed $SNPS.mod2 > $SNPS.rsID.nohead
+sed '1s/^/rsID/' $SNPS.head
+cat $SNPS.head $SNPS.rsID.nohead > $SNPS.rsID
